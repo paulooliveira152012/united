f8855aa2a7c0d053d1d4a70017154ea76f9b1067
paulooliveira152012 <paulooliveira152012@gmail.com>
2024-08-18 12:37:20 -0300

    user sign up and sign in backedn implemented

--- backend/routes.js
+++ backend/routes.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
+// signup
 router.post('/signup', async (req, res) => {
     try {
         const { firstName, lastName, username, email, password, profileImage, admin } = req.body;
@@ -40,6 +41,61 @@ router.post('/signup', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+// test signup:
+// {
+// 	"firstName" : "Paulo",
+// 	"lastName" : "Oliveira",
+// 	"username" : "paulooliveira152012",
+// 	"email" : "paulo@gmail.com",
+// 	"admin" : "true",
+// 	"password" : "123456789"
+// }
+
+// login
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        
+        if(!email || !password) {
+            return res.status(400).json({ message: 'Email and password are required' });
+        }
+
+        // validate the presence of email and password
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        // compare the porvided password with stored password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' })
+        }
+
+        // convert user document to a plain object and remove password
+        const userObject = user.toObject();
+        delete userObject.password;
+
+        console.log('User Object from the backend login route is: ', userObject)
+        // login successful
+        res.status(200).json(userObject);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+});
+
+// test login:
+// 	"email" : "paulo@gmail.com",
+// 	"password" : "123456789"
+
+// video Listing for my play list
+// video fetching
+// user info update
+// account delete
+
+
 
 // Export the router so it can be used in server.js
 module.exports = router;
